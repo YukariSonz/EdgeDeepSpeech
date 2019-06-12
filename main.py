49a01c2b516a2736eae4868ec87d6f0daf2f76a7
Yilei Liang <sonzbreaker@hotmail.com>
2019-06-12 15:47:03 +0100

    Fixed bugs

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 from argparse import ArgumentParser, SUPPRESS
 import logging as log
 
-from openvino.inference_engine import IENetwork, IEPlugin
+from armv7l.openvino.inference_engine import IENetwork, IEPlugin
 
 
 
@@ -14,11 +14,11 @@ def build_argparser():
     parser = ArgumentParser(add_help=False)
     args = parser.add_argument_group('Options')
     args.add_argument('-h','--help',action='help', default=SUPPRESS, help='Show this help message and exit')
-    args.add_argument('-m','--model',help="Required. Path to .xml file with a trained model"), required=True, type = str)
+    args.add_argument('-m','--model',help="Required. Path to .xml file with a trained model", required=True, type = str)
     args.add_argument('-i','--input',help="Required. Path to speech file", required = True, type=str)
     args.add_argument('-l','--cpu_extension',help="Optional. Required for CPU custom layers. Absolute path to a shared library with the kernels implementations.",
         type = str, default=None)
-    args.add_argument('-pp','plugin_dir',help="Optional. Path to a plugin folder", type=str, default=None)
+    args.add_argument('-pp','--plugin_dir',help="Optional. Path to a plugin folder", type=str, default=None)
     args.add_argument('-d','--device',help="Optional. Specify the target device to infer on. Options: Intel CPU, GPU, FPGA, HDDL or MYRAID (Nerual Compute Stick) "
         "is acceptable. The program will look for a suitable plugin for device specified. Default Value: CPU", default="CPU",type=str)
     args.add_argument('--labels',help="Optional. Path to labels mapping file", default=None, type=str)
@@ -26,9 +26,9 @@ def build_argparser():
     return parser
 
 def sound_decode(file):
-    data,samplerate = sf.read(file,always_2d=True,out=np.ndarray)
-    return data,samplerate
-
+    data,samplerate = sf.read(file,always_2d=True)
+    data = data.sum(axis=1)/2
+    return data
 
 
 def main():
@@ -46,7 +46,7 @@ def main():
     net = IENetwork(model = model_xml, weights = model_bin)
     if plugin.device == "CPU":
         supported_layers = plugin.get_supported_layers(net)
-        not_supported_llayers = [l or l in net.layers.keys() if l not in supported_layers]
+        not_supported_layers = [l for l in net.layers.keys() if l not in supported_layers]
         if len(not_supported_layers) !=0:
             log.error("Some layers are not supported by the plugin")
             sys.exit(1)
@@ -61,11 +61,13 @@ def main():
 
 
 
-    exec_net = plugin.load(network = net, num_requsts = 3)
+    exec_net = plugin.load(network = net, num_requests = 3)
 
     #exec_net.requests[0].inputs['data'][:] = voice
     res = exec_net.infer({'data': voice})
-    res
+    print("haha")
 
     
 
+if __name__ == "__main__":
+    sys.exit(main() or 0)
